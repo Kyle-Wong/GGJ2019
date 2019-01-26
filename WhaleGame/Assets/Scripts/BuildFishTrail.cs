17e6ba0e26b59b263fe594361d8f5a573562658c
Kyle Wong <kylewonga@gmail.com>
2019-01-25 22:47:04 -0800

    Added build/follow trail script

---
+++ WhaleGame/Assets/Scripts/BuildFishTrail.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildFishTrail : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static List<Vector3> Trail;
+    public int MaxLength;
+
+    void Awake()
+    {
+        Trail = new List<Vector3>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Trail.Add(transform.position);
+        if(Trail.Count >= MaxLength){
+            Trail.RemoveAt(0);
+        }
+    }
+    
+    void OnDrawGizmos()
+    {
+        
+        Gizmos.color = Color.green;
+        for(int i = 0; i < Trail.Count; i++)
+        {
+            Gizmos.DrawWireSphere(Trail[i],0.5f);
+        }
+    }
+}
---
+++ WhaleGame/Assets/Scripts/FollowTrail.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTrail : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int index;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(BuildFishTrail.Trail.Count > index)
+            transform.position = BuildFishTrail.Trail[index];
+    }
+}
