c42f30b8880c29321efa16c1d3b6f6e83d5c9aa1
Kyle Wong <kylewonga@gmail.com>
2019-01-26 11:16:31 -0800

    Added whale movement script

---
+++ WhaleGame/Assets/Scripts/WhaleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Camera MainCam;
+    public float RotationSpeed;
+    public float MoveSpeed;
+    private float CurrentAngle;
+    private Rigidbody rb;
+    void Awake()
+    {
+        MainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.RotateTowards(Quaternion.Euler(0,0,CurrentAngle),Quaternion.Euler(0,0,GetAngleToMouse()),RotationSpeed*Time.deltaTime);
+        CurrentAngle = transform.rotation.eulerAngles.z;
+        rb.velocity = transform.right*(-1)*MoveSpeed*Time.deltaTime;
+    }
+    private float GetAngleToMouse()
+    {
+        Vector2 MouseWorldPoint = MainCam.ScreenToViewportPoint(Input.mousePosition);
+        Vector2 MoveVector = new Vector2(0.5f,0.5f)-MouseWorldPoint;
+        return Mathf.Rad2Deg*Mathf.Atan2(MoveVector.y,MoveVector.x);
+    }
+    
+}
