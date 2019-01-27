c1744f9e96a176a234efc1788f04aac16d2ae932
Kyle Wong <kylewonga@gmail.com>
2019-01-27 11:15:07 -0800

    Added game end UI

---
+++ WhaleGame/Assets/Scripts/GameEndUIController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameEndUIController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private EventSystem EventSystem;
+    private Canvas MyCanvas;
+    public GameObject DefaultButton;
+    
+    void Awake()
+    {
+        EventSystem = GameObject.FindGameObjectWithTag("EventSystem").GetComponent<EventSystem>();
+        MyCanvas = GetComponent<Canvas>();
+        MyCanvas.enabled = false;
+    }
+
+    public void ShowUI(bool b)
+    {
+        if(b)
+        {
+            MyCanvas.enabled = true;
+            if(DefaultButton != null)
+                EventSystem.SetSelectedGameObject(DefaultButton);
+        } else {
+            MyCanvas.enabled = false;
+        }
+    }
+    public void MakeButtonActive(GameObject g)
+    {
+        EventSystem.SetSelectedGameObject(g);
+    }
+    public void RetryPress()
+    {
+        StartCoroutine(MainMenuController.TransitionThenLoad("GameScene"));
+    }
+    public void MainMenuPress()
+    {
+        StartCoroutine(MainMenuController.TransitionThenLoad("MainMenu"));
+    }
+}
