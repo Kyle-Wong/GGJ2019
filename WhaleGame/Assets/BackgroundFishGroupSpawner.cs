06ac57a4125c3e91eace7569eb4f3e879457025c
Kyle Wong <kylewonga@gmail.com>
2019-01-27 14:44:17 -0800

    Added background fish

---
+++ WhaleGame/Assets/BackgroundFishGroupSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFishGroupSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float MinInterval;
+    public float MaxInterval;
+    private float TimeToWait;
+    public float YRange;
+    public Transform FishGroupPrefab;
+    public bool FacingRight;
+    void Start()
+    {
+        StartCoroutine(SpawnLoop());
+    }
+
+    // Update is called once per frame
+    private IEnumerator SpawnLoop()
+    {
+        while(true){
+            SpawnFishGroup();
+            yield return new WaitForSeconds(Random.Range(MinInterval,MaxInterval));
+        }   
+    }
+    private void SpawnFishGroup()
+    {
+        Transform t = Instantiate(FishGroupPrefab,transform.position,Quaternion.identity);
+        t.position += Vector3.up*Random.Range(-YRange,YRange);
+        t.GetComponent<ConstantMovement>().XVelocity = -12;
+        if(FacingRight)
+            t.GetComponent<ConstantMovement>().XVelocity *= -1;
+        Destroy(t.gameObject,15);
+    }
+}
---
+++ WhaleGame/Assets/Scripts/ConstantMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float XVelocity;
+    public float VerticalPeriodSpeed;
+    public float VerticalAmplitude;
+    private float Timer;
+
+    void Start()
+    {
+        Timer = 0;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime*VerticalPeriodSpeed;
+        transform.position += Vector3.right*XVelocity*Time.deltaTime;
+        transform.position += Vector3.up*Mathf.Sin(Timer)*VerticalAmplitude*Time.deltaTime;
+    }
+}
