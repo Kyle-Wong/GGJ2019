ec1bbb556191bf0f662f8ae0c3e0caa98fc02c21
Xavier Chua <xchua@uci.edu>
2019-01-26 23:20:12 -0800

    Made FishType Script and added type scripts to the prefab variants

---
+++ WhaleGame/Assets/Scripts/FishType.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FishTypes { Nemo, Dory, Sun };
+
+public class FishType : MonoBehaviour
+{
+
+    public FishTypes myType;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
