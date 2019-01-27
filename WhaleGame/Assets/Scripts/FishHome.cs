e0e9291a7e4a9101aa23fb71ab8ee972c994ce6d
Xavier Chua <xchua@uci.edu>
2019-01-26 16:16:48 -0800

    Create fishHome prefab
    Also added script inside oceanmanager for spawning fish homes randomly.

---
+++ WhaleGame/Assets/Scripts/FishHome.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishHome : MonoBehaviour
+{
+
+    public float lifetime = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, lifetime);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
