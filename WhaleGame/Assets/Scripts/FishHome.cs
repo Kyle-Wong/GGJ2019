40dc5f21e2417591cc83cc016561636cd29f8031
Xavier Chua <xchua@uci.edu>
2019-01-27 14:46:32 -0800

    FishHome changed
    Gets model specific to fish type

--- WhaleGame/Assets/Scripts/FishHome.cs
+++ WhaleGame/Assets/Scripts/FishHome.cs
@@ -8,69 +8,59 @@ public class FishHome : MonoBehaviour
 {
 
     public float lifetime = 30f;
+    public Mesh[] meshes;
+    public Material[] materials;
+
 
     [SerializeField] public FishTypes myType;
 
     MeshRenderer meshRender;
-    private Material[] mats;
-    private Material n_mat;
+    MeshFilter filter;
+
     private AudioSource Source;
     // Start is called before the first frame update
     void Start()
     {
         Source = GetComponent<AudioSource>();
         meshRender = this.GetComponent<MeshRenderer>();
-
-        mats = meshRender.materials;
+        filter = this.GetComponent<MeshFilter>();
 
         Destroy(this.gameObject, lifetime);
-        
+
         myType = (FishTypes)Random.Range(0, 4);
-        /*
-        switch(myType)
+        switch (myType)
         {
             case FishTypes.Dory:
-                n_mat = mats[0];
-                break;
-
-            case FishTypes.Sun:
-                n_mat = mats[1];
+                filter.mesh = meshes[0];
+                filter.transform.rotation = Quaternion.identity;
+                filter.transform.localScale = new Vector3(50f, 40f);
+                meshRender.material = materials[0];
                 break;
 
             case FishTypes.Nemo:
-                n_mat = mats[2];
+                filter.mesh = meshes[1];
+                meshRender.material = materials[1];
+                break;
+
+            case FishTypes.Sun:
+                filter.mesh = meshes[2];
+                meshRender.material = materials[2];
                 break;
 
             case FishTypes.Hairtail:
-                n_mat = mats[3];
+                filter.mesh = meshes[3];
+                meshRender.material = materials[3];
                 break;
         }
-        meshRender.material = n_mat;
-        mesh*/
-
     }
 
-    // Update is called once per frame
-    void Update()
+        // Update is called once per frame
+        void Update()
     {
 
     }
-
-    /*
-    private void ChangeMaterials(string colorName)
-    {
-        for(int i = 0; i < mats.Length; i++)
-        {
-            if (mats[i].name == colorName)
-            {
-                Material temp = mats[0];
-                meshRender.material = mats[i];
-                mats[i] = temp;
-            }
-        }
-    }*/
-
-    private void OnTriggerEnter(Collider other)
+    
+    void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.tag + " Collided");
         if (other.CompareTag("Fish"))
@@ -88,7 +78,7 @@ public class FishHome : MonoBehaviour
 
     }
 
-    private void OnDestroy()
+    void OnDestroy()
     {
         OceanManager.RemoveFishHome(this.gameObject);   
     }
