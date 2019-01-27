586692a7d540e8adf5464c0d3b8585fd4bc51a8b
Brian Lam <BrianLam4101@gmail.com>
2019-01-26 18:30:45 -0800

    Water Follows Camera

---
+++ WhaleGame/Assets/WaterFollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFollowCamera : MonoBehaviour
+{
+    private Transform cameraTransform;
+
+    private void Start() {
+        cameraTransform = Camera.main.transform;
+    }
+
+    private void LateUpdate() {
+        transform.position = new Vector3(cameraTransform.position.x, transform.position.y, transform.position.z);
+    }
+}
