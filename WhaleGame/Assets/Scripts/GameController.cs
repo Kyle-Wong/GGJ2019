61bf8db085a7fd4dc0f2a4b758a484aaf7775d27
Kyle Wong <kylewonga@gmail.com>
2019-01-26 22:35:43 -0800

    added game timer and game controller

---
+++ WhaleGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GameOver = false;
+    public float GameDuration;
+    [HideInInspector]
+    public float GameTimer;
+    void Start()
+    {
+        GameOver = false;
+        GameTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameTimer += Time.deltaTime;
+        if(GameTimer > GameDuration)
+        {
+            GameOver = true;
+        }
+    }
+}
---
+++ WhaleGame/Assets/Scripts/GameTimerUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimerUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameController gc;
+        public GameObject InnerBar;
+
+    private RectTransform Rect;
+    private Image Image;
+
+    
+    public Color[] Colors;
+    public Text text;
+    public string[] Phrases;
+
+    void Start()
+    {
+        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        Rect = InnerBar.GetComponent<RectTransform>();
+        Image = InnerBar.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float progression = gc.GameTimer/gc.GameDuration;
+        Rect.anchorMax = new Vector2(Mathf.Lerp(0,1,progression),1);
+        if(progression > .8f)
+        {
+            text.text = Phrases[2];
+            Image.color = Colors[2];
+        } else if(progression > 0.4f)
+        {
+            text.text = Phrases[1];
+            Image.color = Colors[1];
+        } else{
+            text.text = Phrases[0];
+            Image.color = Colors[0];
+        }
+    }
+}
