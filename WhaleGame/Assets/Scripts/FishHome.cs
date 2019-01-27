b087ef8b2ff6947aa6d1f206aa2cc8db64c33467
Xavier Chua <xchua@uci.edu>
2019-01-27 11:31:02 -0800

    Fish homes remove from ocean manager list of all fish homes

--- WhaleGame/Assets/Scripts/FishHome.cs
+++ WhaleGame/Assets/Scripts/FishHome.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum FishTypes { Nemo, Dory, Sun};
+public enum FishTypes { Nemo, Dory, Sun, Hairtail};
 
 public class FishHome : MonoBehaviour
 {
@@ -20,24 +20,50 @@ public class FishHome : MonoBehaviour
         meshRender = this.GetComponent<MeshRenderer>();
 
         mats = meshRender.materials;
-        Debug.Log(mats[0]+" "+ mats[1] +" " + mats[2]);
+
         Destroy(this.gameObject, lifetime);
 
-        myType = (FishTypes)Random.Range(0, 3);
+        myType = (FishTypes)Random.Range(0, 4);
 
         if(myType == FishTypes.Dory)
         {
             meshRender.material = mats[0];
+            Debug.Log("Dory home");
+            foreach (Material m in mats)
+            {
+                Debug.Log(m.name);
+            }
         }
 
         if(myType == FishTypes.Nemo)
         {
+
             meshRender.material = mats[1];
+            Debug.Log("Nemo home");
+            foreach (Material m in mats)
+            {
+                Debug.Log(m.name);
+            }
         }
 
         if(myType == FishTypes.Sun)
         {
             meshRender.material = mats[2];
+            Debug.Log("Sun home");
+            foreach (Material m in mats)
+            {
+                Debug.Log(m.name);
+            }
+        }
+
+        if (myType == FishTypes.Hairtail)
+        {
+            meshRender.material = mats[3];
+            Debug.Log("Sun home");
+            foreach (Material m in mats)
+            {
+                Debug.Log(m.name);
+            }
         }
 
 
@@ -66,5 +92,10 @@ public class FishHome : MonoBehaviour
             }
         }
 
+    }
+
+    private void OnDestroy()
+    {
+        OceanManager.RemoveFishHome(this.gameObject);   
     }
 }
